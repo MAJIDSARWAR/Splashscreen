c996f68b07f2b58e45599522e9a2180d783812b6
AL-HAMD COMPUTERS <majidsarwar30@gmail.com>
2018-04-02 16:54:49 +0500

    Initial commit

---
+++ app/src/main/java/xample/al_hamdcomputerse/splashscreen/Splash_screen.java
@@ -0,0 +1,35 @@
+package xample.al_hamdcomputerse.splashscreen;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+  Thread th=new Thread(){
+      @Override
+      public void run(){
+          try {
+              sleep(2000);
+          } catch (InterruptedException e) {
+              e.printStackTrace();
+          }
+          finally {
+              Intent obj=new Intent(Splash_screen.this,MainActivity.class);
+          startActivity(obj);
+          }
+      }
+
+  };
+th.start();
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        finish();
+    }
+}
